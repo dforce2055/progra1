f613d0cfd1bd312579ede0a0068e5e6f9922ab3e
Diego Pérez <dperez2055@gmail.com>
2015-09-02 12:11:54 -0300

    Corrección en el espaciodo, de 4 a 2 espacios

--- 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
+++ 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
@@ -1,24 +1,24 @@
-/* 7.	Hacer una función que permita insertar una sub-cadena en una cadena a partir de una posición dada. */
+/* 7.	Hacer una funciÃ³n que permita insertar una sub-cadena en una cadena a partir de una posiciÃ³n dada. */
 /* tratar de rehacerlo utilizando metodo de corrimientos */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 char *insertSubCadena(char *destino, char *origen, int posicion);
 int main(){
-	char cadena[100] = "hola";
-	char sub[] = " mundo";
-	char *pCadena = insertSubCadena(cadena, sub, 31);
+  char cadena[100] = "hola";
+  char sub[] = " mundo";
+  char *pCadena = insertSubCadena(cadena, sub, 31);
 	
-	if(pCadena) puts(pCadena);
-	else puts("error, la posicion excede el limite del arreglo");
+  if(pCadena) puts(pCadena);
+  else puts("error, la posicion excede el limite del arreglo");
 
-	return 0;
+  return 0;
 }
 char *insertSubCadena(char *destino, char *origen, int posicion){
-	char *pDestino = destino;
-	if((int)strlen(destino) >= posicion) {
-		pDestino = pDestino + posicion;
-		strcpy(pDestino, origen);
-	}else destino = '\0';
-	return destino;
+  char *pDestino = destino;
+  if((int)strlen(destino) >= posicion) {
+    pDestino = pDestino + posicion;
+    strcpy(pDestino, origen);
+  }else destino = '\0';
+  return destino;
 }
