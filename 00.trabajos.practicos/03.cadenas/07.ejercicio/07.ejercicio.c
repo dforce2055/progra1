2cf26356be82888fea8459f29ade06a1a6af0051
Diego Pérez <dperez2055@gmail.com>
2015-09-02 18:56:52 +0000

    mejora del algoritmo 07.ejercicio

--- 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
+++ 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
@@ -1,4 +1,4 @@
-/* 7.	Hacer una funciÃ³n que permita insertar una sub-cadena en una cadena a partir de una posiciÃ³n dada. */
+/* 7. Hacer una función que permita insertar una sub-cadena en una cadena a partir de una posición dada. */
 /* tratar de rehacerlo utilizando metodo de corrimientos */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -8,7 +8,7 @@ int main(){
   char cadena[100] = "hola";
   char sub[] = " mundo";
   char *pCadena = insertSubCadena(cadena, sub, 31);
-	
+  
   if(pCadena) puts(pCadena);
   else puts("error, la posicion excede el limite del arreglo");
 
