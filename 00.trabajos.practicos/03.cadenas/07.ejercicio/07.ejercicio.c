bda34657c1534e471942862d14287c1344fe292f
Diego Pérez <dperez2055@gmail.com>
2015-09-02 14:20:47 +0000

    mejoramos el algoritmo del 07.ejercicio

--- 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
+++ 00.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
@@ -3,9 +3,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+char *insertSubCadena(char *destino, char *origen, int posicion);
 int main(){
 	char cadena[100] = "hola";
-	strcat(cadena, " mundo");
-	puts(cadena);
+	char sub[] = " mundo";
+	char *pCadena = insertSubCadena(cadena, sub, 31);
+	
+	if(pCadena) puts(pCadena);
+	else puts("error, la posicion excede el limite del arreglo");
+
 	return 0;
+}
+char *insertSubCadena(char *destino, char *origen, int posicion){
+	char *pDestino = destino;
+	if((int)strlen(destino) >= posicion) {
+		pDestino = pDestino + posicion;
+		strcpy(pDestino, origen);
+	}else destino = '\0';
+	return destino;
 }
