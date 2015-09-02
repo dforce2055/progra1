b453f3a22865f757abc6c9634e1a73270b252042
Diego Pérez <dperez2055@gmail.com>
2015-09-02 18:57:19 +0000

    agrego solución al 08.ejercicio

---
+++ 00.trabajos.practicos/03.cadenas/08.ejercicio/08.ejercicio.c
@@ -0,0 +1,22 @@
+/* 8. Hacer una función que permita eliminar una sub-cadena a partir de una posición y un tamaño dados. */
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+void eliminarSub(char *cadena, char *subCadena, int posicion);
+int main(){
+  char cadena[100] = "Esto es un placer horror!";
+  char subCadena[] = " horror";
+  char subCadena2[] = "placer";
+  eliminarSub(cadena, subCadena, 17);
+  puts(cadena);
+
+  return 0;
+}
+void eliminarSub(char *cadena, char *subCadena, int posicion){
+  char *pCadena = cadena + posicion;
+  char *pSubCadena = pCadena + strlen(subCadena);
+
+  if(strcmp(pCadena, subCadena))
+    for( ; *pSubCadena != '\0'; pCadena++, pSubCadena++) *pCadena = *pSubCadena;
+    *pCadena = '\0';
+}
