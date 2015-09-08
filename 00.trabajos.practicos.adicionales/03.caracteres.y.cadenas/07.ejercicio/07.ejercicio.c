6bdd4c0f58006ea13b4e1d438772b0ef8f585e1a
Diego Pérez <dperez2055@gmail.com>
2015-09-08 00:11:21 +0000

    resolución 07.ejercicio cadenas adicionales

---
+++ 00.trabajos.practicos.adicionales/03.caracteres.y.cadenas/07.ejercicio/07.ejercicio.c
@@ -0,0 +1,29 @@
+/* Codificar  una  función  strsub(s,d,n)   que  devuelva  una  subcadena  de  
+la  cadena  de caracteres  s,  indicando  en  d  la  posición  inicial  y  en  
+n  la  cantidad  de  caracteres  deseada. 
+Ejemplo,  dada  la  secuencia:  "El  número  de  teléfono  es  4356-7890."  se  
+desea  obtener  el teléfono, que está a partir de la posición 26 y tiene 9 
+caracteres.  */
+#include <stdio.h>
+#include <string.h>
+char *strsub(char *s,int d,int n);
+int main(){
+  char cadena[256] = "El  número  de  teléfono  es  4356-7890.";
+  char *subCadena = strsub(cadena, 32, 9);
+  
+  if(subCadena[0] != '\0') puts(subCadena);
+  else puts("Error");
+  return 0;
+}
+char *strsub(char *s,int d,int n){
+  char *pSubCadena = s;
+  char *pFinSubCadena;
+  
+  if(strlen(s) > d + n){//si el largo de la cadena es mayor posición+cant.caracteres
+    pSubCadena = pSubCadena+d;//corro hasta llegar a la posición cadena+d
+    pFinSubCadena = pSubCadena+n;//le asigno la dirección final a pSubCadena
+    *pFinSubCadena = '\0';
+  }else *pSubCadena = '\0';
+  
+  return(pSubCadena);
+}
