2bf6e136311caccb9b823f912d531f05c5690e58
dforce2055 <dperez2055@gmail.com>
2015-09-09 13:01:32 -0300

    correcta resolución del 07.ejercicio de cadenas

--- 01.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
+++ 01.trabajos.practicos/03.cadenas/07.ejercicio/07.ejercicio.c
@@ -1,25 +1,25 @@
-/* 7. Hacer una función que permita insertar una sub-cadena en una cadena a 
+/* 7. Hacer una función que permita insertar una sub-cadena en una cadena a
 partir de una posición dada. */
 /* tratar de rehacerlo utilizando metodo de corrimientos */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-char *insertSubCadena(char *destino, char *origen, int posicion);
+int insertSubCadena(char *destino, char *origen, int posicion);
 int main(){
   char cadena[100] = "hola";
   char sub[] = " mundo";
-  char *pCadena = insertSubCadena(cadena, sub, 31);
-  
-  if(pCadena) puts(pCadena);
+
+  if(insertSubCadena(cadena, sub, 2+2)) puts(cadena);
   else puts("error, la posicion excede el limite del arreglo");
 
   return 0;
 }
-char *insertSubCadena(char *destino, char *origen, int posicion){
+int insertSubCadena(char *destino, char *origen, int posicion){
   char *pDestino = destino;
+  int resultado = 1;
   if((int)strlen(destino) >= posicion) {
     pDestino = pDestino + posicion;
     strcpy(pDestino, origen);
-  }else destino = '\0';
-  return destino;
+  }else resultado = 0;
+  return resultado;
 }
