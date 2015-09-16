8e10a3a7d4b03dc568883912e1eceb0dbf269940
Diego Pérez <dperez2055@gmail.com>
2015-09-16 19:08:09 +0000

    Primer parcial oficial Progra1

---
+++ 02.parciales/1er.parcial/00.parcial.oficial.c
@@ -0,0 +1,18 @@
+/* Ingresar a través del teclado una frase cualquiera, la que podrá tener hasta 
+un máximo de 100 palabras. Luego se solicita desarrollar un programa que 
+realice las siguientes tareas.
+
+  1. Mostrar la frase por pantalla, paro encerrado entre paréntesis aquellas 
+  palabras donde la cantidad de vocales supere el 50% del total de los 
+  caracteres de la misma. 
+
+  2. Imprimir por pantalla las palabras que cumplan con la condición indicada 
+  en el punto anterior, mostrándolas en orden alfabético y sin paréntesis.
+
+Nota:
+  # Se considera palabra a un conjunto de uno o más caracteres seguidos de uno o 
+    más espacios al principio o al final.
+    
+  # Las vocales con tilde no se consideran.
+
+*/
