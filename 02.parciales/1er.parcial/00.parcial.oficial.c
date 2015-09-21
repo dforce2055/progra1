2b6e1680959a79c18f27512ae049d197693a1abd
Diego Pérez <dperez2055@gmail.com>
2015-09-21 19:10:01 +0000

    agrego modificaciones en 1er parcial

--- 02.parciales/1er.parcial/00.parcial.oficial.c
+++ 02.parciales/1er.parcial/00.parcial.oficial.c
@@ -14,5 +14,47 @@ Nota:
     más espacios al principio o al final.
     
   # Las vocales con tilde no se consideran.
-
 */
+#pragma warning(disable:4996)
+#pragma warning(disable:638)
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int leerCadena(char *cadena, int n);
+int main(){
+  char frase[1000];
+
+  
+  if(leerCadena(frase,1000)){
+    puts("Usted dijo:");
+    puts(frase);
+  }else puts("error al leer la cadena");
+
+  return 0;
+}
+int leerCadena(char *cadena, int n) {
+    int i, c;
+
+    c = getchar();
+    if (c == EOF) {
+        cadena[0] = '\0';
+        return 0;
+    }
+
+    if (c == '\n')
+        i = 0;
+    else {
+        cadena[0] = c;
+        i = 1;
+    }
+
+    for (; i < n-1 && (c=getchar())!=EOF && c!='\n'; i++)
+       cadena[i] = c;
+
+    cadena[i] = '\0';
+
+    if (c != '\n' && c != EOF)
+        while ((c = getchar()) != '\n' && c != EOF);
+
+    return 1;
+}
