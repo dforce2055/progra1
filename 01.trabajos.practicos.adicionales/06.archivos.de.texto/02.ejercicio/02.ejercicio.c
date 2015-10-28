0561ad6191abc63aa0b77bfb65cc7edaf662dfd3
dforce2055 <dperez2055@gmail.com>
2015-10-28 19:59:29 -0300

    resolución 02.ejercicio archivos de texto adicionales

--- 01.trabajos.practicos.adicionales/06.archivos.de.texto/02.ejercicio/02.ejercicio.c
+++ 01.trabajos.practicos.adicionales/06.archivos.de.texto/02.ejercicio/02.ejercicio.c
@@ -5,11 +5,13 @@ formato:
   Promedio
 */
 #include <stdio.h>
+#include <string.h>
 int main(){
   FILE *in, *out;
   char nombre[80];
   char nota[80];
-  int n1, n2, promedio;
+  int n1, n2;
+  float promedio;
 
   if((in = fopen("notas.txt","rt")) == NULL){
     puts("No pudo leerse notas.txt");
@@ -22,12 +24,14 @@ int main(){
 
   fgets(nombre, 79, in);
   while(!feof(in)){
+    //nombre[strlen(nombre)-1] = '\0';
     fgets(nota, 79, in);
     sscanf(&nota[4], "%d", &n1);
     fgets(nota, 79, in);
     sscanf(&nota[4], "%d", &n2);
     promedio = (n1 + n2) / 2;
-    printf("%s %d %d %d", nombre, n1, n2, promedio);
+    printf("%s %d %d %.2f\n", nombre, n1, n2, promedio);
+    fprintf(out,"%s%.2f\n", nombre, promedio);
     fgets(nombre, 79, in);
   }
 
