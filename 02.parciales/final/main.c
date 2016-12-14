f5eae4d5bad710af88fe83f348fb58f3f4514018
Diego Pérez <dperez2055@gmail.com>
2016-12-14 14:19:20 -0300

    Final de Programación 1

--- 02.parciales/final/main.c
+++ 02.parciales/final/main.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "encabezado.h"
+int main(){
+  Pelicula peliculas[CANTIDAD] = {0};
+  int opcion = 1, posicion = 10;
+
+
+  peliculas[0] = agregarPelicula("Rocky I", "Silvester Stallone", "ACCION", 1, 1976);
+  peliculas[1] = agregarPelicula("Rocky II", "Silvester Stallone", "ACCION", 2, 1979);
+  peliculas[2] = agregarPelicula("Rocky III", "Silvester Stallone", "ACCION", 10, 1983);
+  peliculas[3] = agregarPelicula("Rocky IV", "Silvester Stallone", "ACCION", 3, 1990);
+  peliculas[4] = agregarPelicula("Rocky V", "Silvester Stallone", "ACCION", 10, 1994);
+  peliculas[5] = agregarPelicula("Rocky VI", "Silvester Stallone", "ACCION", 12, 2006);
   peliculas[6] = agregarPelicula("The Shawshank Redemption", "Frank Darabont", "DRAMA", 10, 1994);
   peliculas[7] = agregarPelicula("The Godfather", "Francis Ford Coppola", "DRAMA", 10, 1972);
   peliculas[8] = agregarPelicula("The Godfather: Part II", "Francis Ford Coppola", "DRAMA", 10, 1974);
